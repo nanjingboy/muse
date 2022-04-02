97d195727427c08731eff5fb8d044c6717c6cc2e
Tom.Huang <hzlhu.dargon@gmail.com>
2022-04-02 16:23:32 +0800

    feat(macros): add StrEnum proc macro

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,11 +1,19 @@
 mod int_enum;
+mod str_enum;
 
 use int_enum::int_enum_impl;
 use proc_macro::TokenStream;
+use str_enum::str_enum_impl;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(IntEnum, attributes(int_enum))]
 pub fn int_enum(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     TokenStream::from(int_enum_impl(&ast))
+}
+
+#[proc_macro_derive(StrEnum)]
+pub fn str_enum(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    TokenStream::from(str_enum_impl(&ast))
 }
