b6af799c0b96e8d512b14b88ce96ff4f3dff10f8
Tom.Huang <hzlhu.dargon@gmail.com>
2022-04-08 15:49:13 +0800

    feat(macros): add StructRefCellSetter proc macro

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,9 +1,11 @@
 mod int_enum;
 mod str_enum;
+mod struct_ref_cell_setter;
 
 use int_enum::int_enum_impl;
 use proc_macro::TokenStream;
 use str_enum::str_enum_impl;
+use struct_ref_cell_setter::struct_ref_cell_setter_impl;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(IntEnum, attributes(int_enum))]
@@ -16,4 +18,10 @@ pub fn int_enum(input: TokenStream) -> TokenStream {
 pub fn str_enum(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     TokenStream::from(str_enum_impl(&ast))
+}
+
+#[proc_macro_derive(StructRefCellSetter, attributes(struct_ref_cell_setter))]
+pub fn struct_ref_cell_setter(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    TokenStream::from(struct_ref_cell_setter_impl(&ast))
 }
