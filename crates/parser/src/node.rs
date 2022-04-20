1028fd0b251d13217e59d74f54c5ae852fb4d12f
Tom.Huang <hzlhu.dargon@gmail.com>
2022-04-20 10:00:03 +0800

    feat(parser): complete logics for node

--- crates/parser/src/node.rs
+++ crates/parser/src/node.rs
@@ -16,13 +16,17 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn new(parser: &Parser, pos: i32, loc: &Position) -> Self {
+    pub fn new(parser: &Parser, pos: i32, loc: &Option<Position>) -> Self {
         Node {
             node_type: "".to_string(),
             start: pos,
             end: 0,
             loc: if parser.options.locations {
-                Some(SourceLocation::new(loc, &None, &parser.source_file))
+                if let Some(loc) = loc {
+                    Some(SourceLocation::new(loc, &None, &parser.source_file))
+                } else {
+                    None
+                }
             } else {
                 None
             },
@@ -32,6 +36,56 @@ impl Node {
             } else {
                 None
             },
+        }
+    }
+}
+
+pub trait NodeParser {
+    fn start_node(&self) -> Node;
+    fn start_node_at(&self, pos: i32, loc: &Option<Position>) -> Node;
+    fn finish_node(&self, node: &mut Node, node_type: &str);
+    fn finish_node_at(&self, node: &mut Node, node_type: &str, pos: i32, loc: &Option<Position>);
+}
+
+impl NodeParser for Parser {
+    fn start_node(&self) -> Node {
+        self.start_node_at(
+            self.cur_token_start.borrow().clone(),
+            &self.cur_token_start_loc.borrow().clone(),
+        )
+    }
+
+    fn start_node_at(&self, pos: i32, loc: &Option<Position>) -> Node {
+        Node::new(self, pos, loc)
+    }
+
+    fn finish_node(&self, node: &mut Node, node_type: &str) {
+        self.finish_node_at(
+            node,
+            node_type,
+            self.last_token_end.borrow().clone(),
+            &self.last_token_end_loc.borrow().clone(),
+        );
+    }
+
+    fn finish_node_at(&self, node: &mut Node, node_type: &str, pos: i32, loc: &Option<Position>) {
+        node.node_type = node_type.to_owned();
+        node.end = pos;
+        if self.options.locations {
+            if let Some(loc) = loc {
+                if let Some(ref current_loc) = node.loc {
+                    node.loc = Some(SourceLocation::new(
+                        &current_loc.start,
+                        &Some(loc.clone()),
+                        &self.source_file,
+                    ));
+                }
+            }
+        }
+        if self.options.ranges {
+            if let Some((start_range, _)) = node.range {
+                node.range = Some((start_range, pos));
+            }
         }
     }
 }
